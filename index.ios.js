68605afda7498eff3878fb189a66b9eae962b594
Myles O'Connor <myles@jarvis.local>
2016-10-02 16:23:36 -0700

    Added Redux dependencies

--- index.ios.js
+++ index.ios.js
@@ -16,7 +16,7 @@ class PaperChase extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.welcome}>Welcome to Paperchase!</Text>
                 <Text style={styles.instructions}>To get started, edit index.ios.js</Text>
                 <Text style={styles.instructions}>Press Cmd+R to reload,{'\n'}Cmd+D or shake for dev menu</Text>
             </View>
